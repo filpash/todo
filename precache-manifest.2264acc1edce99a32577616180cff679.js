885a2322906edde96aebe3c00a6a37d643202edb
Filipp <fili92@mail.ru>
2020-01-04 01:33:52 +0300

    Updates

---
+++ precache-manifest.2264acc1edce99a32577616180cff679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce42e0510209cfaa21b7458be6d12a67",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "bfd76cfac38924627637",
+    "url": "/todo/static/css/main.5e0081d2.chunk.css"
+  },
+  {
+    "revision": "4fd7c4c34293b67452f6",
+    "url": "/todo/static/js/2.91eeb5cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.91eeb5cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfd76cfac38924627637",
+    "url": "/todo/static/js/main.ffa4f6e7.chunk.js"
+  },
+  {
+    "revision": "9e3d90c4d53deff53c8a",
+    "url": "/todo/static/js/runtime-main.48ad9b76.js"
+  }
+]);
